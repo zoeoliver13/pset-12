5580ef6377994ae4e5f918528312aed81f26305e
Ryan Wilson <ryan.wilson@empiriolabs.io>
2021-02-23 10:40:39 -0500

    initial commit

---
+++ src/ProblemSet12.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class ProblemSet12 {
+
+    public long factorial(int n) {
+
+    }
+
+    public long fibonacci(int n) {
+
+    }
+
+    public long triangle(int rows) {
+
+    }
+
+    public long sumDigits(int n) {
+
+    }
+
+    public long powerN(int base, int exponent) {
+
+    }
+
+    public String changeXY(String text) {
+
+    }
+
+    public int array11(int[] data, int index) {
+
+    }
+
+    public int strCount(String text, String sub) {
+
+    }
+
+    public boolean strCopies(String text, String sub, int n) {
+
+    }
+
+    public int strDist(String text, String sub) {
+
+    }
+}
